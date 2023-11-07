d4d9d39480dac31f2a28d647c7b3e5c2ce6d8fba
Luis Martínez <luismg78@hotmail.com>
2023-11-06 18:07:43 -0600

    .

---
+++ LAMBusiness.Shared/DTO/Bitacora/BitacoraExcepcionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LAMBusiness.Shared.DTO.Bitacora
+{
+    using Shared.Aplicacion;
+
+    public class BitacoraExcepcionDTO : Bitacora
+    {
+        public string Excepcion { get; set; }
+        public string ErrorAlGuardarBitacoraDB { get; set; }
+    }
+}
